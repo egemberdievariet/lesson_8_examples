d4d162f0ad8ea90d5152830d8b3cb86e3c8b3f48
Egemberdievariet <57054923+egemberdievariet@users.noreply.github.com>
2021-10-22 19:58:54 +0600

    change last

--- bin/week8.dart
+++ bin/week8.dart
@@ -1,11 +1,36 @@
 void main(List<String> arguments) {
-  double mul(double a, int b) {
-    double c;
-    c = a + b;
-    // print("Sum : $c");
-    return c;
-  }
+  // print(task1(5));
+  // print(task2(3, 5));
+  // print(task3(4, 2, 2));
+  print(name1([2, 3, 2, 5, 6]));
+}
 
-  print(mul(1, 56));
-  print(mul(78, 1342));
+double task1(double a) {
+  double b;
+  b = a * a;
+  // print("Sum : $b");
+  return b;
+}
+
+int task2(int a, int b) {
+  int c = a + b;
+  return c;
+}
+
+double task3(double a, double b, double c) {
+  double d;
+  d = (a - b) / c;
+  return d;
+}
+
+String name1(List list1) {
+  // List list1 = [];
+  for (int i = 0; i < list1.length; i++) {
+    if (list1[i] == list1[i + 1]) {
+      print("da");
+    } else {
+      print("net");
+    }
+  }
+  return ("");
 }
